db4d345522598ef39f0a498b24aaf555be4d4aea
kanishma99 <kanishma.kani99@gmail.com>
2025-04-29 16:53:31 +0530

    New method add Thiru

---
+++ src/test/java/git/Sample.java
@@ -0,0 +1,19 @@
+package git;
+
+public class Sample {
+	
+	private void git1() {
+		System.out.println("1");
+	}
+
+	private void git2() {
+		System.out.println("2");
+
+	}
+
+	private void git3() {
+		System.out.println("3");
+
+	}
+
+}
